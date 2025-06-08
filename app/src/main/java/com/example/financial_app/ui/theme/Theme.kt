5d0b4c449edf17748317f9a6d88e1dc1003d489f
Semenov A.A <mike123777mike@gmail.com>
2025-06-08 23:42:53 +0300

    Создал каркас приложения и описал цветовую палитру и шрифты

---
+++ app/src/main/java/com/example/financial_app/ui/theme/Theme.kt
@@ -0,0 +1,58 @@
+package com.example.financial_app.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val LightColorScheme = lightColorScheme(
+    primary = Green,
+    onPrimary = DarkestGray,
+    inversePrimary = RealWhite,
+    primaryContainer = LightGreen,
+    onPrimaryContainer = Black,
+
+    secondaryContainer = Red,
+    onSecondaryContainer = RealWhite,
+
+    surface = White,
+    onSurface = Black,
+    onSurfaceVariant = DarkestGray,
+    inverseSurface = LightGreen,
+    surfaceContainerLowest = RealWhite,
+    surfaceContainerLow = LightestGray,
+    surfaceContainer = LightGray,
+
+    outline = DarkGray,
+    outlineVariant = Gray
+)
+
+private val DarkColorScheme = LightColorScheme
+// TODO: Заменить на инициализацию через darkColorScheme, когда будет задание сделать темную тему
+
+@Composable
+fun Financial_appTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
