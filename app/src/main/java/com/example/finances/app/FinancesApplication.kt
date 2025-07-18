1ddaec97b8975bbe5be2838b97a901e2ec790eca
Semenov A.A <mike123777mike@gmail.com>
2025-07-18 22:50:37 +0300

    воркер ожил

--- app/src/main/java/com/example/finances/app/FinancesApplication.kt
+++ app/src/main/java/com/example/finances/app/FinancesApplication.kt
@@ -2,8 +2,14 @@ package com.example.finances.app
 
 import android.app.Application
 import android.content.Context
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.finances.core.di.AppComponent
 import com.example.finances.core.di.DaggerAppComponent
+import com.example.finances.core.managers.LogMessageWorker
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.util.concurrent.TimeUnit
 
 class FinancesApplication : Application() {
     lateinit var appComponent: AppComponent
@@ -12,6 +18,26 @@ class FinancesApplication : Application() {
         super.onCreate()
         appComponent = DaggerAppComponent.factory().create(this)
         appComponent.networkConnectionObserver()
+        
+        scheduleLogMessageWorker()
+    }
+    
+    private fun scheduleLogMessageWorker() {
+        val targetTime = LocalDateTime.now()
+            .withHour(22)
+            .withMinute(40)
+            .withSecond(0)
+        
+        val currentTime = LocalDateTime.now()
+        val delay = targetTime.toEpochSecond(ZoneOffset.UTC) - currentTime.toEpochSecond(ZoneOffset.UTC)
+        
+        if (delay > 0) {
+            val workRequest = OneTimeWorkRequestBuilder<LogMessageWorker>()
+                .setInitialDelay(delay, TimeUnit.SECONDS)
+                .build()
+                
+            WorkManager.getInstance(this).enqueue(workRequest)
+        }
     }
 }
 
